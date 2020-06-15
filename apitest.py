7743f22edf82b2ed23672e703acd1323ee99e0be
Andrew Teeter <ateeter@umass.edu>
2020-06-15 03:56:47 -0400

    download to folder

--- apitest.py
+++ apitest.py
@@ -2,8 +2,20 @@ from steam.client import SteamClient
 from csgo.client import CSGOClient
 from csgo.features import Match
 import csgo.sharecode
-from csgo.protobufs.cstrike15_gcmessages_pb2 import CMsgGCCStrike15_v2_MatchList
 import json
+import urllib.request
+import requests
+import shutil
+
+# site= "https://csgostats.gg/match"
+# hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+#        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+#        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+#        'Accept-Encoding': 'none',
+#        'Accept-Language': 'en-US,en;q=0.8',
+#        'Connection': 'keep-alive'}
+
+# req = urllib.request.Request(site, headers=hdr)
 
 client = SteamClient()
 cs = CSGOClient(client)
@@ -18,17 +30,20 @@ def start_csgo():
 @cs.on('ready')
 def gc_ready():
     print('Ready')
-    # meme = csgo.sharecode.decode('CSGO-y7k25-vtEun-8NBMO-NmYLV-7FFdH')
-    # print(meme)
-    # cs.request_full_match_info(meme['matchid'], meme['outcomeid'], meme['token'])
-    # response, = cs.wait_event('full_match_info')
-    #print(response)
+    meme = csgo.sharecode.decode('CSGO-y7k25-vtEun-8NBMO-NmYLV-7FFdH')
+    print(meme)
+    cs.request_full_match_info(meme['matchid'], meme['outcomeid'], meme['token'])
+    response, = cs.wait_event('full_match_info')
+    site = response.matches[0].roundstatsall[-1].map
+    file_name = f"{meme['matchid']}_{meme['outcomeid']}_{meme['token']}"
+    print(site)
+    print(file_name)
+    with urllib.request.urlopen(site) as res, open(file_name, 'wb') as out_file:
+        shutil.copyfileobj(res, out_file)
 
-    cs.request_player_profile(375771776)
-    response, = cs.wait_event('player_profile')
-    print(response)
     cs.exit()
+    cs.emit('meme')
 
 
-client.cli_login(username=auth['usr'],password=auth['pass'])
-client.run_forever()
+client.login(username=auth['usr'],password=auth['pass'])
+cs.wait_event('meme')
--- getMatches.py
+++ getMatches.py
@@ -1,5 +1,11 @@
+from steam.client import SteamClient
+from csgo.client import CSGOClient
+from csgo.features import Match
+import csgo.sharecode
+import json
 import urllib.request
 import requests
+import shutil
 from bs4 import BeautifulSoup
 
 site= "https://csgostats.gg/match"
@@ -10,6 +16,8 @@ hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML,
        'Accept-Language': 'en-US,en;q=0.8',
        'Connection': 'keep-alive'}
 
+codes = []
+
 req = urllib.request.Request(site, headers=hdr)
 
 try:
@@ -18,22 +26,55 @@ except urllib.error.HTTPError as e:
     print(e.fp.read())
 
 soup = BeautifulSoup(page, "html.parser")
-with open('ShareCodes', 'a') as f:
-    for icon in soup.find_all("span", class_="glyphicon glyphicon-play-circle"):
-        matchURL = "https://csgostats.gg" + icon.find_parent("a").get('href')
-        req = urllib.request.Request(matchURL, headers=hdr)
-        try:
-            matchPage = urllib.request.urlopen(req)
-        except urllib.error.HTTPError as e:
-            print(e.fp.read())
-        soup = BeautifulSoup(matchPage, "html.parser")
-        demoURL = "https://csgostats.gg" + soup.find("span", class_="glyphicon glyphicon-facetime-video").find_parent("a").get("href")
-        req = urllib.request.Request(demoURL, headers=hdr)
-        try:
-            demoRes = urllib.request.urlopen(req)
-        except urllib.error.HTTPError as e:
-            demoRes = e.fp.read()
-        if demoRes:
-            soup = BeautifulSoup(demoRes, "html.parser")
-            matchid = soup.find("a").get('href')
-            f.write(matchid[-len('CSGO-AKxQC-ftWKD-LAFyz-FQ8AH-CcyMM'):] + '\n')
+for icon in soup.find_all("span", class_="glyphicon glyphicon-play-circle"):
+    matchURL = "https://csgostats.gg" + icon.find_parent("a").get('href')
+    req = urllib.request.Request(matchURL, headers=hdr)
+    try:
+        matchPage = urllib.request.urlopen(req)
+    except urllib.error.HTTPError as e:
+        print(e.fp.read())
+    soup = BeautifulSoup(matchPage, "html.parser")
+    demoURL = "https://csgostats.gg" + soup.find("span", class_="glyphicon glyphicon-facetime-video").find_parent("a").get("href")
+    req = urllib.request.Request(demoURL, headers=hdr)
+    try:
+        demoRes = urllib.request.urlopen(req)
+    except urllib.error.HTTPError as e:
+        demoRes = e.fp.read()
+    if demoRes:
+        soup = BeautifulSoup(demoRes, "html.parser")
+        matchid = soup.find("a").get('href')
+        codes.append(matchid[-len('CSGO-AKxQC-ftWKD-LAFyz-FQ8AH-CcyMM'):])
+
+
+
+client = SteamClient()
+cs = CSGOClient(client)
+
+with open('./auth.json') as f:
+  auth = json.load(f)
+
+@client.on('logged_on')
+def start_csgo():
+    cs.launch()
+
+@cs.on('ready')
+def gc_ready():
+    print('Ready')
+    for code in codes:
+        meme = csgo.sharecode.decode(code)
+        print(meme)
+        cs.request_full_match_info(meme['matchid'], meme['outcomeid'], meme['token'])
+        response, = cs.wait_event('full_match_info')
+        site = response.matches[0].roundstatsall[-1].map
+        file_name = f"{meme['matchid']}_{meme['outcomeid']}_{meme['token']}"
+        print(site)
+        print(file_name)
+        with urllib.request.urlopen(site) as res, open(file_name, 'wb') as out_file:
+            shutil.copyfileobj(res, out_file)
+
+    cs.exit()
+    cs.emit('meme')
+
+
+client.login(username=auth['usr'],password=auth['pass'])
+cs.wait_event('meme')
